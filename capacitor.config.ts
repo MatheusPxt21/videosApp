e50400f7c5ce3526bf99657cc3bd039db02b1fdf
Matheus Peixoto <mateusnascimentopxt@gmail.com>
2023-01-07 15:49:14 -0300

    alteracoes nas 3 tabs

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'videosApp',
+  appName: 'MyApp',
   webDir: 'www',
   bundledWebRuntime: false
 };
