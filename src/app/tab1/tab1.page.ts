66d9eef604ca0f9bcefeffd932d50d0f62559b81
Matheus Peixoto <mateusnascimentopxt@gmail.com>
2023-01-13 01:09:41 -0300

    varias mudancas-home,alertas...

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +10,72 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController, public toastController: ToastController) {}
+
+  async AlertaCurtirFilme() {
+    const alert = await this.alertController.create({
+      header: 'Alerta de Confirmação!',
+      message: 'Realmente deseja CURTIR esse filme?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          },
+        },
+        {
+          text: 'SIM, CURTIR',
+          handler: () => {
+            //console.log('Confirm Okay');
+            this.presentToast();
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+
+  async AlertaDescurtirFilme() {
+    const alert = await this.alertController.create({
+      header: 'Alerta de Confirmação!',
+      message: 'Realmente deseja DESCURTIR esse filme?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          },
+        },
+        {
+          text: 'SIM, DESCURTIR',
+          handler: () => {
+            /*console.log('Confirm Okay');*/
+            this.presentToast();
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Solicitação Confirmada',
+      duration: 2000,
+      animated: true,
+      color: 'light',
+      position: 'bottom'
+
+
+    });
+    toast.present();
+  }
 
 }
